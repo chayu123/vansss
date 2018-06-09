13f99f31c189366a3bba503cdfd4bc15c846f1f1
zy <996975903@qq.com>
2018-06-09 18:23:46 +0800

    这是还剩头部的首页

--- js/home.js
+++ js/home.js
@@ -1,15 +1,12 @@
-// window.onscroll=function(){
-// 		var scroll = document.body.scrollTop || document.documentElement.scrollTop;
-// 		var str = document.getElementById('logmiddle');
-// 		if(scroll>=200){
-// 			str.style.position="fixed";
-// 			str.style.left="0";
-// 			str.style.top="0";
-// 		}else{
-// 			str.style.position="relative";
-// 		}
-// 	}
-
-
+$(window).scroll(function(){
+		var scroll = document.body.scrollTop || document.documentElement.scrollTop;
+		
+		if(scroll>=70){
+			$('#box1').css({position:"fixed",margin:"0 auto",top:"0"});
+			
+		}else{
+			$('#box1').css("position","relative");
+		}
+	});
 
 
